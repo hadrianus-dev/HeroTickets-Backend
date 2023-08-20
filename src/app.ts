ca7b81a081eced6d8d22d5d4848cdd2cd2708dcb
David Hadrianus <95863744+hadrianus-dev@users.noreply.github.com>
2023-08-20 21:46:37 +0100

    app is running

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
+import express, { Application } from "express"
+
 class App {
+    public app: Application
+
+    constructor(){
+        this.app = express()
+    }
     
+    listen(){
+        this.app.listen(3333, () => console.log('server is running'))
+    }
 }
 
 export {App};
