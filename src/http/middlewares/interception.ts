469c5e0b6c58f59165a65d2b01cf7f9d91c962e4
David Hadrianus <95863744+hadrianus-dev@users.noreply.github.com>
2023-08-20 23:33:45 +0100

    refactory number type on status enviroment

--- src/http/middlewares/interception.ts
+++ src/http/middlewares/interception.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 
 export function error(err: HttpException, req: Request, res: Response, next: NextFunction): void
 {
-    const status: Number = err.status;
+    const status: number = err.status;
     const message: string = err.message;
 
     res.status(status).json({
